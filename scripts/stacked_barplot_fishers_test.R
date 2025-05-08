eee711ab2e559791aab83608924239445527a85e
Selam Mehreteab <smehrete@ucsc.edu>
2025-05-08 15:16:46 -0700

    Update stacked_barplot_fishers_test.R

--- scripts/stacked_barplot_fishers_test.R
+++ scripts/stacked_barplot_fishers_test.R
@@ -13,7 +13,7 @@ xtab <- as.table(rbind( c(209,51), c(104,12), c(180,79), c(212,92), c(636,298), 
 dimnames(xtab) <- list( Class = c('Alternative acceptor', 'Alternative donor', 'Alternative first exon', 'Alternative last exon', 'Cassette', 'Coordinate cassette', 'Mutually exclusive'), Mutation = c("U2AF1 S34F LACZ", "U2AF1 S34F KRAS G12V"))
 
 
-xtab. <- row_wise_fisher_test(xtab)
+xtab <- row_wise_fisher_test(xtab)
 
 write.csv(as.data.frame(xtab), file='~/Desktop/stacked_barplot_fishers_test')
 
