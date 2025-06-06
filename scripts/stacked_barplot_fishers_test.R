e665f7a8ccb7e814f316448b586d274f73bde63b
Selam Mehreteab <smehrete@ucsc.edu>
2025-06-06 08:06:05 -0700

    Update scripts/stacked_barplot_fishers_test.R
    Co-authored-by: Cindy Liang <67753617+cindyeliang@users.noreply.github.com>

--- scripts/stacked_barplot_fishers_test.R
+++ scripts/stacked_barplot_fishers_test.R
@@ -10,7 +10,7 @@ library(rstatix)
 xtab <- as.table(rbind( c(209,51), c(104,12), c(180,79), c(212,92), c(636,298), c(18,1), c(7,3), c(44,0)))
 
 
-dimnames(xtab) <- list( Class = c('Alternative acceptor', 'Alternative donor', 'Alternative first exon', 'Alternative last exon', 'Cassette', 'Coordinate cassette', 'Mutually exclusive'), Mutation = c("U2AF1 S34F LACZ", "U2AF1 S34F KRAS G12V"))
+dimnames(xtab) <- list( Class = c('Alternative acceptor', 'Alternative donor', 'Alternative first exon', 'Alternative last exon', 'Cassette', 'Coordinate cassette', 'Mutually exclusive', 'Intron retention'), Mutation = c("U2AF1 S34F LACZ", "U2AF1 S34F KRAS G12V"))
 
 
 xtab <- row_wise_fisher_test(xtab)
